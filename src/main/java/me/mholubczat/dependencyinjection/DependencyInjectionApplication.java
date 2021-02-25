d20fafee4f07bd78b43cc48d66b92aead8b1593e
m.holubczat <m.holubczat@gmail.com>
2021-02-25 22:13:26 +0100

    initial commit

---
+++ src/main/java/me/mholubczat/dependencyinjection/DependencyInjectionApplication.java
@@ -0,0 +1,22 @@
+package me.mholubczat.dependencyinjection;
+
+import me.mholubczat.dependencyinjection.controller.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx =  SpringApplication.run(DependencyInjectionApplication.class, args);
+
+		MyController myController =(MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
+	}
+
+}
---
+++ src/main/java/me/mholubczat/dependencyinjection/controller/MyController.java
@@ -0,0 +1,13 @@
+package me.mholubczat.dependencyinjection.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        System.out.println("Hello World!!!");
+
+        return "Hi Folks!";
+    }
+}
